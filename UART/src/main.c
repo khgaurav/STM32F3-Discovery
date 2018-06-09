d326c58c4e79606bcb6cb9dcf76b058a70352fa2
Gaurav K H <gaurav.kh1999@gmail.com>
2018-06-09 16:43:07 +0530

    First Commit

---
+++ UART/src/main.c
@@ -0,0 +1,30 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f3xx.h"
+#include "stm32f3_discovery.h"
+			
+
+int main(void)
+{
+	UART_HandleTypeDef UartHandle;
+	UartHandle.Instance        = USART2;
+	UartHandle.Init.BaudRate     = 9600;
+	UartHandle.Init.WordLength   = UART_WORDLENGTH_8B;
+	UartHandle.Init.StopBits     = UART_STOPBITS_1;
+	UartHandle.Init.Parity       = UART_PARITY_NONE;
+	UartHandle.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
+	UartHandle.Init.Mode         = UART_MODE_TX_RX;
+	UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
+	UartHandle.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
+	for(;;);
+}

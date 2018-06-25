919862ddac3af9752732850c1ca3f4f5c9ad110e
Gaurav K H <gaurav.kh1999@gmail.com>
2018-06-25 12:42:16 +0530

    UART commit

--- UART/src/main.c
+++ UART/src/main.c
@@ -15,8 +15,21 @@
 
 int main(void)
 {
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_USART2_CLK_ENABLE();
+	__HAL_RCC_GPIOE_CLK_ENABLE();
+
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.Pin			= GPIO_PIN_2 | GPIO_PIN_3 ;
+	GPIO_InitStruct.Mode		= GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Speed		= GPIO_SPEED_FREQ_MEDIUM;
+	GPIO_InitStruct.Pull		= GPIO_NOPULL;
+	GPIO_InitStruct.Alternate	= GPIO_AF7_USART2;
+
+	HAL_GPIO_Init(GPIOA,&GPIO_InitStruct);
+
 	UART_HandleTypeDef UartHandle;
-	UartHandle.Instance        = USART2;
+	UartHandle.Instance			 = USART2;
 	UartHandle.Init.BaudRate     = 9600;
 	UartHandle.Init.WordLength   = UART_WORDLENGTH_8B;
 	UartHandle.Init.StopBits     = UART_STOPBITS_1;
@@ -25,6 +38,15 @@ int main(void)
 	UartHandle.Init.Mode         = UART_MODE_TX_RX;
 	UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
 	UartHandle.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
+	HAL_UART_Init(&UartHandle);
+	NVIC_EnableIRQ(USART2_IRQn);
+	uint8_t data=30;
+
+	HAL_UART_Transmit(&UartHandle,&data,0x100,0x1FFFFFF);
+	BSP_LED_Init(LED9);
+	BSP_LED_On(LED9);
+	HAL_UART_Receive(&UartHandle,&data,0x100,0x1FFFFFF);
 
 	for(;;);
 }
